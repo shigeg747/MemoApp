db69fdad3ccf17f70c55ae2c4787804ed182f3aa
Shigeru Ohara <74123345+shigeg747@users.noreply.github.com>
2021-03-19 01:03:33 +0900

    Install Eslint

--- App.jsx
+++ App.jsx
@@ -6,7 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* eslint-disable-next-line */}
+      <StatusBar style='auto' />
     </View>
   );
 }
